2dc368a4e4ea14fa2059d9fda7e8754231ee6cf1
henrypapadatos <papadatoshenry@gmail.com>
2024-01-25 18:59:23 -0800

    Add OOD tests while training probe

--- probe/train_probe.py
+++ probe/train_probe.py
@@ -120,12 +120,41 @@ def prepare_dataset_anthropic(dataset_file: str, split: float, size: int, layer:
     labels_test = labels[int(split*size):]
 
     return inputs_train, labels_train, inputs_test, labels_test
+
+#%%
+def prepare_POL_dataset(size: int, layer: int):
+    #load the dataframe
+    data_folder = "../datasets"
+    df = pd.read_pickle(os.path.join(data_folder, 'POL_sycophantic_activations.pkl'))
+
+    #not that the datapoints go pairwise. I.e. the first 2 rows with will have the exact same quesiotns asked in the prompt, only the answer from the assistant will be different
+    #thereofre we randomly select datapoint 2 by 2 to have a balanced dataset
+    df = df[df['activations'].notna()]
+    indexes = random.sample(range(0, len(df)//2), size//2)
+
+    inputs = []
+    labels = []
+    for index in indexes:
+        inputs.append(df.iloc[index*2]['activations'][layer])
+        inputs.append(df.iloc[index*2+1]['activations'][layer])
+        labels.append(df.iloc[index*2]['sycophancy'])
+        labels.append(df.iloc[index*2+1]['sycophancy'])
+
+    return inputs, labels
+
+def prepare_open_ended_dataset(layer: int):
+
+    df = pd.read_pickle('../datasets/open_ended_activations.pkl')
+    #take the activations of the right layer and put them on the right device
+    df['activations_sycophantic'] = df['activations_sycophantic'].apply(lambda x: x[layer])
+    df['activations_not_sycophantic'] = df['activations_not_sycophantic'].apply(lambda x: x[layer])
+    return df
 #%% 
 def setup_wandb(config):
     # Start a new run, tracking hyperparameters in config
     run = wandb.init(
         # Set the project where this run will be logged
-        project="probe_training_MRPC_RT_NLP",
+        project="probe_training_NLP_RT",
         # #set the name of the run
         # name=f"probe_{config['activation_layer']}",
         # Track hyperparameters
@@ -210,7 +239,86 @@ def get_probe_accuracy(probe, loss_fn, inputs_test, labels_test, batch_size: int
     return test_accuracy, test_loss
 
 #%%
-def train_probe(probe, loss_fn, optimizer, inputs_train, labels_train, inputs_test, labels_test, epochs: int, batch_size: int = 8, use_wandb: bool = False, verbose: bool = False):
+def get_POL_performances(probe, inputs_POL, labels_POL, batch_size: int = 8):
+    #remove the sigmoid from the probe
+    probe = probe[:-1]
+    #create a dataloader for the test dataset
+    POL_dataloader = torch.utils.data.DataLoader(list(zip(inputs_POL, labels_POL)), batch_size=batch_size)
+
+    num_sycophantic = 0
+    num_not_sycophantic = 0
+    sycophantic_score = 0
+    not_sycophantic_score = 0
+
+    with torch.no_grad():
+        for inputs, labels in POL_dataloader:
+            
+            #move the inputs and labels to cuda if available
+            device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            #get the predictions
+            outputs = probe(inputs).squeeze()
+
+            for pred, label in zip(outputs, labels):
+                if label == 1:
+                    num_sycophantic += 1
+                    sycophantic_score += pred.item()
+
+                else:
+                    num_not_sycophantic += 1
+                    not_sycophantic_score += pred.item()  
+
+    diff = sycophantic_score/num_sycophantic - not_sycophantic_score/num_not_sycophantic
+    sycophantic_score = sycophantic_score/num_sycophantic
+    not_sycophantic_score = not_sycophantic_score/num_not_sycophantic
+
+    return diff, sycophantic_score, not_sycophantic_score
+
+def get_open_ended_performances(probe, open_ended_df):
+
+    mean_sycophantic_score = []
+    mean_not_sycophantic_score = []
+    mean_sycophantic_agreeing_type = []
+    mean_not_sycophantic_agreeing_type = []
+    mean_sycophantic_disagreeing_type = []
+    mean_not_sycophantic_disagreeing_type = []
+
+    probe = probe[:-1]
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    with torch.no_grad():
+        for idx, row in open_ended_df.iterrows():
+            activations = row['activations_sycophantic'].to(device)
+            score = probe(activations).mean().item()
+            mean_sycophantic_score.append(score)
+            if row['agreeing_type'] == 1:
+                mean_sycophantic_agreeing_type.append(score)
+            else:
+                mean_sycophantic_disagreeing_type.append(score)
+
+            activations = row['activations_not_sycophantic'].to(device)
+            score = probe(activations).mean().item()
+            mean_not_sycophantic_score.append(score)
+            if row['agreeing_type'] == 1:
+                mean_not_sycophantic_agreeing_type.append(score)
+            else:
+                mean_not_sycophantic_disagreeing_type.append(score)
+        
+        diff = np.median(np.subtract(mean_sycophantic_score, mean_not_sycophantic_score))
+        diff_agreeing_type = np.median(np.subtract(mean_sycophantic_agreeing_type, mean_not_sycophantic_agreeing_type))
+        diff_disagreeing_type = np.median(np.subtract(mean_sycophantic_disagreeing_type, mean_not_sycophantic_disagreeing_type))
+
+        return diff, diff_agreeing_type, diff_disagreeing_type
+
+#%%
+def get_open_endend_performances(probe, layer):
+
+    df = pd.read_pickle('../datasets/NLP_sycophantic_activations.pkl')
+
+#%%
+def train_probe(probe, loss_fn, optimizer, inputs_train, labels_train, inputs_test, labels_test, epochs: int, batch_size: int = 8, use_wandb: bool = False, verbose: bool = False,
+                inputs_POL = None, labels_POL = None, open_ended_df = None):
     """
     Train the probe model
     """
@@ -222,6 +330,8 @@ def train_probe(probe, loss_fn, optimizer, inputs_train, labels_train, inputs_te
     #compute the test accuracy before training
     train_accuracy, train_loss = get_probe_accuracy(probe, loss_fn, inputs_train, labels_train, batch_size=batch_size)
     test_accuracy, test_loss = get_probe_accuracy(probe, loss_fn, inputs_test, labels_test, batch_size=batch_size)
+    POL_diff, POL_sycophantic_score, POL_not_sycophantic_score = get_POL_performances(probe, inputs_POL, labels_POL, batch_size=batch_size)
+    open_ended_diff, open_ended_agreeing_type_diff, open_ended_disagreeing_type_diff = get_open_ended_performances(probe, open_ended_df)
     if  use_wandb:
             wandb.log({"initial_train_loss": train_loss, 
                     "initial_train_accuracy": train_accuracy, 
@@ -230,7 +340,13 @@ def train_probe(probe, loss_fn, optimizer, inputs_train, labels_train, inputs_te
                     "train_loss": train_loss,
                     "train_accuracy": train_accuracy,
                     "test_loss": test_loss,
-                    "test_accuracy": test_accuracy})
+                    "test_accuracy": test_accuracy,
+                    "POL_diff": POL_diff,
+                    "POL_sycophantic_score": POL_sycophantic_score,
+                    "POL_not_sycophantic_score": POL_not_sycophantic_score,
+                    "open_ended_diff": open_ended_diff,
+                    "open_ended_agreeing_type_diff": open_ended_agreeing_type_diff,
+                    "open_ended_disagreeing_type_diff": open_ended_disagreeing_type_diff})
 
     #train the model
     for epoch in range(epochs):
@@ -264,13 +380,22 @@ def train_probe(probe, loss_fn, optimizer, inputs_train, labels_train, inputs_te
 
         #compute the test accuracy
         test_accuracy, test_loss = get_probe_accuracy(probe, loss_fn, inputs_test, labels_test, batch_size=batch_size)
+        POL_diff, POL_sycophantic_score, POL_not_sycophantic_score = get_POL_performances(probe, inputs_POL, labels_POL, batch_size=batch_size)
+        open_ended_diff, open_ended_agreeing_type_diff, open_ended_disagreeing_type_diff = get_open_ended_performances(probe, open_ended_df)
+
         #log the train loss to wandb
         if  use_wandb:
             wandb.log({"train_loss": train_loss, 
                       "train_accuracy": train_accuracy, 
                       "test_loss": test_loss, 
                       "test_accuracy": test_accuracy, 
-                      "epoch": epoch+1})
+                      "epoch": epoch+1,
+                      "POL_diff": POL_diff,
+                      "POL_sycophantic_score": POL_sycophantic_score,
+                      "POL_not_sycophantic_score": POL_not_sycophantic_score,
+                      "open_ended_diff": open_ended_diff,
+                      "open_ended_agreeing_type_diff": open_ended_agreeing_type_diff,
+                      "open_ended_disagreeing_type_diff": open_ended_disagreeing_type_diff})
 
         if verbose:
             print(f"Train loss: {train_loss / len(train_dataloader):.3f}")
@@ -341,41 +466,47 @@ def train(config: dict):
                                                                             verbose=True)
     
     print('Size of the train dataset: ', len(inputs_train))
+
+    #create the OOD test dataset
+    inputs_POL, labels_POL = prepare_POL_dataset(size=200, layer=config['activation_layer'])
+    open_ended_df = prepare_open_ended_dataset(layer=config['activation_layer'])
+
     #shuffle the dataset
     #create the probe model
     probe, loss_fn, optimizer = create_probe(number_of_layers=config['number_of_layers'], learning_rate=config['learning_rate'], use_wandb=config['use_wandb'], L2 = config['L2'])
     
     #train the probe model
-    train_probe(probe, loss_fn, optimizer, inputs_train, labels_train, inputs_test, labels_test, epochs=config['epochs'], batch_size=config['batch_size'], use_wandb=config['use_wandb'], verbose=True)
+    train_probe(probe, loss_fn, optimizer, inputs_train, labels_train, inputs_test, labels_test, epochs=config['epochs'], batch_size=config['batch_size'], use_wandb=config['use_wandb'], verbose=True,
+                inputs_POL=inputs_POL, labels_POL=labels_POL, open_ended_df=open_ended_df)
 
     if config['use_wandb']:
         wandb.finish()
     
     if config['save_probe']:
-        probe_name = f"checkpoints/probe_v13_{config['activation_layer']}.pt"
+        probe_name = f"checkpoints/probe_vX_{config['activation_layer']}.pt"
         torch.save(probe, probe_name)
 
 #%%
 if __name__ == "__main__":
 
-    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    os.environ['CUDA_VISIBLE_DEVICES'] = '3'
 
     # for layer in range(32):
     #     #setup the config for wandb
     config={
             "use_wandb": True,
-            "dataset_file": ["NLP_sycophantic_activations.pkl", "rotten_tomatoes_sycophantic_activations.pkl", "MRPC_sycophantic_activations.pkl"],
-            "dataset_size": [2000, 200, 100],
+            "dataset_file": ["NLP_sycophantic_activations.pkl", "rotten_tomatoes_sycophantic_activations.pkl"],
+            "dataset_size": [2000, 200],
             "activation_layer": 18,
             "split_train_test": 0.8,
 
             "number_of_layers": 1,
             "learning_rate": 0.0005,
-            "epochs": 40,
+            "epochs": 120,
             "batch_size": 4,
             "L2": 0.00005,
 
-            "save_probe": True,
+            "save_probe": False,
         }
         
     train(config)
